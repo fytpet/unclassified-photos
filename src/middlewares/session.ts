41af7d1011a3c090e8c4fc00c6fb06985b6a633b
Petros Fytilis <fytpet@gmail.com>
2022-11-13 17:11:35 -0500

    Remove sameSite restriction

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -4,8 +4,7 @@ const ONE_HOUR = 1000 * 60 * 60;
 
 export const session = expressSession({
   cookie: {
-    maxAge: ONE_HOUR,
-    sameSite: true
+    maxAge: ONE_HOUR
   },
   secret: process.env.SESSION_SECRET,
   resave: true,
