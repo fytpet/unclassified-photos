48c640813c11de2ced69ed547936ac6f1df6e447
Petros Fytilis <fytpet@gmail.com>
2023-05-03 19:01:26 -0400

    Remove HTTPS server

--- src/tests/setupProcessEnv.ts
+++ src/tests/setupProcessEnv.ts
@@ -1,8 +1,7 @@
 export default function() {
   process.env.NODE_ENV = "development";
   process.env.BASE_URI = "http://localhost:3000";
-  process.env.HTTP_PORT = "3000";
-  process.env.HTTPS_PORT = "443";
+  process.env.PORT = "3000";
   process.env.GOOGLE_CLIENT_ID = "some.google.client.id";
   process.env.GOOGLE_CLIENT_SECRET = "some.google.client.secret";
   process.env.SESSION_SECRET = "some.session.secret";
