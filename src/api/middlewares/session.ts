9cb2be74b8e1b9b0d7d87070907c4aabba7f8796
Petros Fytilis <fytpet@gmail.com>
2023-05-03 20:27:07 -0400

    Remove secure attribute from cookie

--- src/api/middlewares/session.ts
+++ src/api/middlewares/session.ts
@@ -7,7 +7,6 @@ const ONE_HOUR = 1000 * 60 * 60;
 export const session = expressSession({
   cookie: {
     maxAge: ONE_HOUR,
-    secure: process.env.NODE_ENV === "production",
   },
   name: "sessionId",
   secret: process.env.SESSION_SECRET,
